38f809e990b0df04c0b382c5bb9f8dc8d104f4cf
Yusuf <ybasori@gmail.com>
2019-06-29 19:22:48 +0700

    pluk

--- app/controllers/api/UploadController.js
+++ app/controllers/api/UploadController.js
@@ -8,7 +8,6 @@ const Helper = require(`../../libraries/Helper.js`);
 module.exports = {
     upload: function(req,res){
         mUpload().any()(req, res, function(err){
-            console.log(req.files)
             return res.status(200).json({
                 msg: "successfully uploaded!"
             });
