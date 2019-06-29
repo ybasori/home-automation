6e9bc31f7c720ad0a892ae84bb3bddd950474805
Yusuf <ybasori@gmail.com>
2019-06-29 19:04:57 +0700

    keran

--- app/controllers/api/UploadController.js
+++ app/controllers/api/UploadController.js
@@ -8,6 +8,7 @@ const Helper = require(`../../libraries/Helper.js`);
 module.exports = {
     upload: function(req,res){
         mUpload().any()(req, res, function(err){
+            console.log(req.files)
             return res.status(200).json({
                 msg: "successfully uploaded!"
             });
